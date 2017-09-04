6adb15f81d90c85f1ce4b9a63575646bd71239d2
Dima Bart <dima.bart01@gmail.com>
2017-09-04 10:42:25 -0400

    Initial Commit

---
+++ Bloom/Bloom.h
@@ -0,0 +1,19 @@
+//
+//  Bloom.h
+//  Bloom
+//
+//  Created by Dima Bart on 2017-09-04.
+//  Copyright © 2017 Dima Bart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bloom.
+FOUNDATION_EXPORT double BloomVersionNumber;
+
+//! Project version string for Bloom.
+FOUNDATION_EXPORT const unsigned char BloomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bloom/PublicHeader.h>
+
+
