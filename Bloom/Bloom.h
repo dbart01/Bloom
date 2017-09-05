c23363a84e035366f11e48ee47bafa418ccf8255
Dima Bart <dima.bart01@gmail.com>
2017-09-04 22:43:57 -0400

    Add dedicated macOS scheme.

--- Bloom/Bloom.h
+++ Bloom/Bloom.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Dima Bart. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Bloom.
 FOUNDATION_EXPORT double BloomVersionNumber;
